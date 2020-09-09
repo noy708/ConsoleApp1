69553279bbe85c64d35599d5a0b582e3d945ec9a
Noy <uver5prs@gmail.com>
2020-09-09 22:03:47 +0900

    プロジェクト ファイルを追加します。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            A();
+            B();
+            C();
+            D();
+            E();
+        }
+
+        public static void A()
+        {
+            Console.WriteLine("exesuteA");
+        }
+        public static void B()
+        {
+            Console.WriteLine("exesuteB");
+        }
+        public static void C()
+        {
+            string result;
+            //文字列連結
+
+            // 連結演算子を使ったサンプル
+            string a = "";
+            for (int i = 0; i < 100000; i++)
+            {
+                a = a + "C#";
+            }
+            result = a.ToString();
+
+            /*
+            // StringBuilderを使ったサンプル
+            StringBuilder b = new StringBuilder();
+            for (int i = 0; i < 100000; i++)
+            {
+                b.Append("C#");
+            }
+            result = b.ToString();
+            */
+
+            Console.WriteLine("exesuteC");
+        }
+        public static void D()
+        {
+            Console.WriteLine("exesuteD");
+        }
+        public static void E()
+        {
+            Console.WriteLine("exesuteE");
+        }
+    }
+}
